3dfde52d3223d1bf9601b0aef2f29b0c614fda1d
Jean Torres <88167764+jeantorres1697@users.noreply.github.com>
2025-04-16 21:18:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+// Menu Toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+const navbar = document.querySelector('.navbar');
+
+// Alternar o menu no mobile
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+});
+
+// Mudança de cor da navbar ao rolar a página
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+// Fecha o menu mobile quando o mouse sai de cima dele
+menu.addEventListener('mouseleave', () => {
+    if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
+        menuToggle.classList.remove('active');
+    }
+});
+
+// Função para fechar todos os modais
+function fecharModais() {
+    const modais = document.querySelectorAll('.modal');
+    modais.forEach(modal => {
+        modal.style.display = 'none';
+    });
+}
+
+// Função para abrir o modal específico
+function abrirModal(id) {
+    fecharModais();  // Fecha todos os modais primeiro
+    const modal = document.getElementById(id);
+    if (modal) {
+        modal.style.display = 'flex'; // Exibe o modal selecionado
+    }
+}
+
+// Adicionando os eventos para abrir os modais
+document.querySelectorAll('.menu a').forEach(item => {
+    item.addEventListener('click', (event) => {
+        const modalId = event.target.getAttribute('onclick').split("'")[1];
+        abrirModal(modalId);
+    });
+});
+
+// Fechar o modal ao clicar fora
+document.querySelectorAll('.modal').forEach(modal => {
+    modal.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+});
